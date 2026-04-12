0d4b46ea7f185605d8398ec75d0057fdf0b24b91
Oleg Freedom <olegfreedom777@gmail.com>
2026-04-12 10:59:46 +0200

    Change path images architecture

--- app/Services/ImagePathService.php
+++ app/Services/ImagePathService.php
@@ -64,7 +64,7 @@ class ImagePathService implements ImagePathServiceInterface
      */
     public function getThumbnailUrl(Image $image): string
     {
-        return config('app.image_api_url') . '/' . config('image.paths.thumbnails') . '/' . $image->id . '.jpg';
+        return config('app.image_api_url') . '/' . config('image.paths.thumbnails') . '/' . $image->thumbnail_filename;
     }
 
     /**
@@ -72,7 +72,7 @@ class ImagePathService implements ImagePathServiceInterface
      */
     public function getImageUrl(Image $image): string
     {
-        return config('app.image_api_url') . '/' . config('image.paths.images') . '/' . $image->id . '.jpg';
+        return config('app.image_api_url') . '/' . config('image.paths.images') . '/' . $image->filename . '.jpg';
     }
 
     /**
