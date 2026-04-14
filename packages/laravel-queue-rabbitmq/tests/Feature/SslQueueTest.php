623c75e76c468a06bbc145c812e517bd41a1eb37
Oleg Freedom <olegfreedom777@gmail.com>
2026-04-14 16:43:42 +0200

    Fork rabbitmq

---
+++ packages/laravel-queue-rabbitmq/tests/Feature/SslQueueTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace VladimirYuldashev\LaravelQueueRabbitMQ\Tests\Feature;
+
+use PhpAmqpLib\Connection\AMQPSSLConnection;
+
+class SslQueueTest extends TestCase
+{
+    protected bool $interactsWithConnection = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->markTestSkipped();
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('queue.default', 'rabbitmq');
+        $app['config']->set('queue.connections.rabbitmq', [
+            'driver' => 'rabbitmq',
+            'queue' => 'default',
+            'connection' => AMQPSSLConnection::class,
+
+            'hosts' => [
+                [
+                    'host' => getenv('HOST'),
+                    'port' => getenv('PORT_SSL'),
+                    'vhost' => '/',
+                    'user' => 'guest',
+                    'password' => 'guest',
+                ],
+            ],
+
+            'options' => [
+                'ssl_options' => [
+                    'cafile' => getenv('RABBITMQ_SSL_CAFILE'),
+                    'local_cert' => null,
+                    'local_key' => null,
+                    'verify_peer' => true,
+                    'passphrase' => null,
+                ],
+            ],
+
+            'worker' => 'default',
+        ]);
+    }
+
+    public function testConnection(): void
+    {
+        $this->assertInstanceOf(AMQPSSLConnection::class, $this->connection()->getChannel()->getConnection());
+    }
+}
